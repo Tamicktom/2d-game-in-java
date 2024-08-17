af0d0b3139ec5be54e31eb86c64dd32980e43125
Tamicktom <rickpokemon38@gmail.com>
2024-08-17 19:10:00 -0300

    create game tiles

---
+++ src/main/GamePanel.java
@@ -0,0 +1,28 @@
+package src.main;
+
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class GamePanel extends JPanel {
+
+  // SCREEN SETTINGS
+  final int originalTileSize = 16; // 16x16 tile
+  final int scale = 3; // scale of the game
+
+  final int tileSize = originalTileSize * scale; // 48x48 tile
+
+  final int maxScreenColumns = 16; // 16 columns
+  final int maxScreenRows = 12; // 12 rows
+
+  final int screenWidth = tileSize * maxScreenColumns; // 768px
+  final int screenHeight = tileSize * maxScreenRows; // 576px
+
+  public GamePanel() {
+    // set preferred size
+    this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+    this.setBackground(Color.black);
+    this.setDoubleBuffered(true);
+  }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package src.main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+  public static void main(String[] args) {
+    JFrame window = new JFrame();
+
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    window.setResizable(false);
+    window.setTitle("My first Java game");
+
+    // create the game panel
+    GamePanel gamePanel = new GamePanel();
+    window.add(gamePanel);
+
+    window.pack();
+
+    window.setLocationRelativeTo(null); // center the window
+    window.setVisible(true);
+  }
+}
